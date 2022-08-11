0c0a39dcdb88ae1e56776fda231a9b842297c393
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-08-11 10:23:47 +0300

    Responsive nav bar

--- responsiv nav bar/app.js
+++ responsiv nav bar/app.js
@@ -1,14 +1,26 @@
 const navSlide = () => {
-  const bar = document.querySelector('.bar');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const bar = document.querySelector(".bar");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-  bar.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-  });
+  // Toogle Nav
+  bar.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwords ${index / 7 + 1.5}s`;
+    // Animation links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+
+      console.log(index / 7);
+    });
+
+    bar.classList.toggle("toggle");
   });
 };
 
